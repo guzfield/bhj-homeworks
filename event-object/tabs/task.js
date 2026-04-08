0a78279878036626b2d275a6c5b0a6738a3200c0
Guzel <gsabirzianova@gmail.com>
2026-04-08 15:40:33 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+const tabContainers = document.querySelectorAll('.tab__navigation');
+
+tabContainers.forEach(container => {
+    const tabs = container.querySelectorAll('.tab');
+    const contents = container.nextElementSibling.querySelectorAll('.tab__content');
+    
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(t => t.classList.remove('tab_active'));
+            tab.classList.add('tab_active');
+            
+            contents.forEach(c => c.classList.remove('tab__content_active'));
+            contents[index].classList.add('tab__content_active');
+        });
+    });
+});
